b99a40e19c0af25703183c2679875d82886d708f
Jaron Armiger <jaron.armiger@gmail.com>
2022-09-16 21:13:21 -0700

    complete CommentsForm

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-## Helianthes
+# Helianthes
 
 the creative revelations of yours truly
 
-# env vars:
-
-# Stack:
+## env vars:
+* NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+## stack:
 * next.js
 * graphql
 * tailwindcss
---
+++ pages/api/comments.js
@@ -0,0 +1,31 @@
+import {
+  GraphQLClient,
+  gql,
+} from 'graphql-request'
+
+const graphQLAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+
+
+
+export default async function asynchandler(req, res) {
+  const graphQLClient = new GraphQLClient(graphQLAPI, {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`
+    }
+  })
+
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) { id }
+    }
+  `;
+  try {
+    console.log(req.body)
+    const result = await graphQLClient.request(query, req.body)
+
+    return res.status(200).send(result)
+  } catch (error) {
+    console.log('FUCK')
+    console.log(error)
+  }
+}
