c5e92438eb9677656102cf4dce69e0288db2372e
Jaron Armiger <jaron.armiger@gmail.com>
2022-09-16 21:57:11 -0700

    complete FeaturedPosts

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import { Categories, PostCard, PostWidget } from '../components'
 import {
   getPosts,
 } from '../services'
+import {
+  FeaturedPosts,
+} from '../sections'
 
 
 export default function Home({ posts }) {
@@ -12,6 +15,7 @@ export default function Home({ posts }) {
         <title>HELIANTHES</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -10,10 +10,18 @@ import {
     Author,
     Comments,
     CommentsForm,
+    Loader,
 } from '../../components'
+import {
+  useRouter,
+} from 'next/router'
 
 const PostDetails = ({ post }) => {
-  console.log(post)
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <Loader />
+  }
   return (
     <div className='container mx-auto px-10 mb-8'>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
